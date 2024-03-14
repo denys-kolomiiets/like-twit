0c98b5e2251c1eebdff4966454b1ab3f0402be72
Denys Kolomiiets <106629418+DenysKo88@users.noreply.github.com>
2024-03-14 12:19:18 +0200

    + create LoginModal

---
+++ components/modals/LoginModal.tsx
@@ -0,0 +1,51 @@
+import useLoginModal from "@/hooks/useLoginModal";
+import React, { useCallback, useState } from "react";
+import Input from "../Input";
+import Modal from "../Modal";
+
+const LoginModal = () => {
+  const loginModal = useLoginModal();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      //TODO add log in
+      loginModal.onClose();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [loginModal]);
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+        disabled={isLoading}
+      />
+    </div>
+  );
+  return (
+    <Modal
+      disabled={isLoading}
+      onClose={loginModal.onClose}
+      isOpen={loginModal.isOpen}
+      title="Login"
+      actionLabel="Sign In"
+      onSubmit={onSubmit}
+      body={bodyContent}
+    />
+  );
+};
+
+export default LoginModal;
