ef54a59ec3354e563e85aa42fb4cafef5c999796
Denys Kolomiiets <106629418+DenysKo88@users.noreply.github.com>
2024-03-20 18:59:43 +0200

    * comment getServerSideProps

--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -1,26 +1,27 @@
 import Header from "@/components/Header";
 import NotificationsFeed from "@/components/NotificationsFeed";
 import { NextPageContext } from "next";
+
 import { getSession } from "next-auth/react";
 
-export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
+// export async function getServerSideProps(context: NextPageContext) {
+//   const session = await getSession(context);
+//   console.log(session);
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: "/",
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {
-      session,
-    },
-  };
-}
+//   return {
+//     props: {
+//       session,
+//     },
+//   };
+// }
 
 const Notifications = () => {
   return (
